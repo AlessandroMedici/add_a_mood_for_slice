47e0ad4c3057517d659a633c6774380d0825ece3
AlessandroMedici <AlessandroMedici@users.noreply.github.com>
2020-02-16 11:18:32 +0100

    Update RepSlip.py

--- RepSlip.py
+++ RepSlip.py
@@ -114,6 +114,21 @@ print(aRs[2:-6]) # that's for two overlapping code BUT no overlap admitted there
 
 '''
 /usr/bin/python3.8 /home/alex/PycharmProjects/robaGenerica/RepSlip.py
+Traceback (most recent call last):
+  File "/home/alex/PycharmProjects/robaGenerica/RepSlip.py", line 105, in <module>
+    print(aRs[2:-6])  # that's for two overlapping code BUT no overlap admitted there.
+  File "/home/alex/PycharmProjects/robaGenerica/RepSlip.py", line 53, in __getitem__
+    raise Exception('Too more overlapping there!',
+Exception: ('Too more overlapping there!', 'get 2 but only 0 is admitted.')
+[8, 9, 0, 1, 2, 3]
+[]
+[2, 3, 4, 5]
+[4, 5, 6, 7, 8, 9, 0, 1, 2, 3]
+[6, 7, 8, 9, 0, 1]
+[2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3]
+
+
+
 [8, 9, 0, 1, 2, 3]
 []
 [2, 3, 4, 5]
@@ -128,20 +143,5 @@ print(aRs[2:-6]) # that's for two overlapping code BUT no overlap admitted there
 (2, 3, 4, 5)
 (4, 5, 6, 7, 8, 9, 0, 1, 2, 3)
 (6, 7, 8, 9, 0, 1)
-Traceback (most recent call last):
-  File "/home/alex/.local/share/JetBrains/Toolbox/apps/PyCharm-C/ch-0/193.5233.109/plugins/python-ce/helpers/pydev/pydevd.py", line 1434, in _exec
-    pydev_imports.execfile(file, globals, locals)  # execute the script
-  File "/home/alex/.local/share/JetBrains/Toolbox/apps/PyCharm-C/ch-0/193.5233.109/plugins/python-ce/helpers/pydev/_pydev_imps/_pydev_execfile.py", line 18, in execfile
-    exec(compile(contents+"\n", file, 'exec'), glob, loc)
-  File "/home/alex/PycharmProjects/robaGenerica/RepSlip.py", line 97, in <module>
-    print(aRs[2:-6]) # that's for two overlapping code
-  File "/home/alex/PycharmProjects/robaGenerica/RepSlip.py", line 63, in __getitem__
-    raise Exception('Too more overlapping there!', "get "+str(lenResult - self.overlap)+" but only "+str(self.overlap)+" is admitted.")
-Exception: ('Too more overlapping there!', 'get 2 but only 0 is admitted.')
-Exception ignored in: <module 'threading' from '/usr/lib/python3.8/threading.py'>
-Traceback (most recent call last):
-  File "/usr/lib/python3.8/threading.py", line 1388, in _shutdown
-    lock.acquire()
-KeyboardInterrupt: 
 
 Process finished with exit code 1'''
