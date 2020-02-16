c44b376469b9f3949fca7fc5a7591dd75e9ebf07
AlessandroMedici <AlessandroMedici@users.noreply.github.com>
2020-02-16 14:27:56 +0100

    Update RepSlip.py

--- RepSlip.py
+++ RepSlip.py
@@ -4,22 +4,18 @@ class Repslicing():
 
     where:
 
-    object ->   must be an iterable and sequentially object, like a list,
-                a tuple or any self-made object that accepts ":" (slice) operator...
-    maxOverlap more than 0 -> that'is the max overlap admitted
-                maxOverlap = 0 -> no overlap admitted, if any then program raise an Exception
-                maxOverlap = -1 -> check disabled
-                look for example past class declaration.
+    object ->   must be an iterable and sequentiable object, like a list,
+                a tuple or any self-made object that accepts ":" (slice) operator.
+    maxOverlap -> it's the max overlap admitted (default = 0)
+                maxOverlap = -1 -> the check is disabled
+                look the example that's past the class declaration.
                 
 
-    Normally, if you type[a:b:step] with a < b, (step isn't relevant), you obtain a empty element.
-    It's the same if you type type[a:b:step] with a == b
+    Normally, if you type[start:stop:step] with start <= stop, ("step" isn't relevant), you obtain an empty element.
 
-    using this class you get these results:
-
-    result = type[a::step]+type[:b:step]
-
-    obviously if you type a == b you get the entire object but with a partially reversed order:
+    Instead using this class you get these results:
+    result = type[start::step]+type[:stop:step]
+    obviously if you type start == stop in this class you get the entire object but with a partially reversed order:
     
     i.e.:
 
@@ -33,17 +29,17 @@ class Repslicing():
         but if:
         print(aRs[2:-6])
         [2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3]
-        It's a overlapping mode.
+        It's an overlapping mode.
 
     other normally slicing modes operate as usual.
     """
 
     def __init__(self, object, maxOverlap = 0):
         """
-        maxOverlap more than 0 -> that'is the max overlap admitted
-        maxOverlap = 0 -> no overlap admitted, if any then program raise an Exception
-        maxOverlap = -1 -> check disabled
-        look for example past class declaration.
+        maxOverlap => 0 -> that's the max overlap admitted
+        i.e.:  with maxOverlap = 0 -> there's no overlap admitted, if there's any, then the program raises an Exception
+        maxOverlap = -1 -> the check is disabled
+        look at the example that's past the class declaration.
         """
 
         self.object = object
@@ -65,12 +61,12 @@ class Repslicing():
 
             aSet = set(a)
             lenInterS = len(aSet.intersection(set(b)))
-            # -> len of intersection is the len of overlap
+            # -> the lenght of intersection is the lenght of overlap
 
             if self.overlap >= 0 and (self.overlap < lenInterS):
-                raise Exception('Too more overlapping there!',
+                raise Exception('Too many overlapping here!',
                                 "get " + str(lenInterS) + " but only " + str(
-                                    self.overlap) + " is admitted.")
+                                    self.overlap) + " is/are admitted.")
 
             return a + b
 
@@ -79,49 +75,38 @@ class Repslicing():
 # Example code:
 
 a = (i for i in range(10))
-aRs = Repslicing(list(a), 3)
-'''
-above is granted for max 3 overlap on a list object
-'''
-
+aRs = Repslicing(list(a), 3) # here the most overlaps granted from a list object are 3 
 
 print(aRs[-2:-6])
 print(aRs[-6:2])
 print(aRs[2:6])
 print(aRs[4:4])
 print(aRs[6:2])
-print(aRs[2:-6]) # that's for two overlapping code
+print(aRs[2:-6]) # this is for two overlaps
 
 print(2*"\n")
 
 a = (i for i in range(10))
-aRs = Repslicing(list(a), -1)
-'''
-above is stated for no check for overlap
-'''
-
+aRs = Repslicing(list(a), -1) # here there's no check for overlaps
 
 print(aRs[-2:-6])
 print(aRs[-6:2])
 print(aRs[2:6])
 print(aRs[4:4])
 print(aRs[6:2])
-print(aRs[2:-6]) # that's for two overlapping code
+print(aRs[2:-6]) # this is for two overlaps
 
 print(2*"\n")
 
 a = tuple(i for i in range(10))
-aRs = Repslicing(a, 0)
-'''
-above is granted for NO overlap on tuple object
-'''
+aRs = Repslicing(a, 0) # here no overlap is admitted (on tuple object)
 
 print(aRs[-2:-6])
 print(aRs[-6:-2])
 print(aRs[2:6])
 print(aRs[4:4])
 print(aRs[6:2])
-print(aRs[2:-6]) # that's for two overlapping code BUT no overlap admitted there.
+print(aRs[2:-6]) # this is for two overlapping codes BUT no overlap is admitted here.
 
 '''
 /usr/bin/python3.8 /home/alex/PycharmProjects/robaGenerica/RepSlip.py
@@ -150,9 +135,9 @@ print(aRs[2:-6]) # that's for two overlapping code BUT no overlap admitted there
 (6, 7, 8, 9, 0, 1)
 Traceback (most recent call last):
   File "/home/alex/PycharmProjects/robaGenerica/RepSlip.py", line 105, in <module>
-    print(aRs[2:-6])  # that's for two overlapping code BUT no overlap admitted there.
+    print(aRs[2:-6])  # this is for two overlapping codes BUT no overlap is admitted here.
   File "/home/alex/PycharmProjects/robaGenerica/RepSlip.py", line 53, in __getitem__
-    raise Exception('Too more overlapping there!',
-Exception: ('Too more overlapping there!', 'get 2 but only 0 is admitted.')
+    raise Exception('Too many overlapping here!',
+Exception: ('Too many overlapping there!', 'get 2 but only 0 is/are admitted.')
 
 Process finished with exit code 1'''
